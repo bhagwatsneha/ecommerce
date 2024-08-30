efb5de9f06982859c5461990b2d4fdcfe8d4c038
bhagwatsneha <snehabhagwat1992@gmail.com>
2024-08-30 20:26:02 +0530

    Add files via upload

---
+++ Selenium Starter codde/MYSeleniumProject/src/com/lavatech/www/Test.java
@@ -0,0 +1,10 @@
+package com.lavatech.www;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+
+	}
+
+}
---
+++ Selenium Starter codde/Mysecondselenium/src/com/abc/www/Sampleclass.java
@@ -0,0 +1,9 @@
+package com.abc.www;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+System.out.println("first project");
+	}
+
+}
